960e2e5cb645d93d6dffaab9716f5014f08d6090
Iven Schlenther <i.schlenther@muensmedia.de>
2024-09-18 14:43:36 +0200

    Apply 2 suggestion(s) to 2 file(s)

--- app/app/App/Console/Kernel.php
+++ app/app/App/Console/Kernel.php
@@ -18,7 +18,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->call(new InvalidateAccessTokensJob)->hourly();
     }
 
     /**
